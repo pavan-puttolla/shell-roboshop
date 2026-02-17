7c06f12f41a942fe6dd63d19663d9eddc50ff42d
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-17 18:07:34 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -7,12 +7,12 @@ for instance in $0
 do      
    instance_id=$(
     aws ec2 run-instances \
-        --image-id "$AMI_ID" \
-        --instance-type "t3.micro" \
-        --security-group-ids "$SG_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-        --query 'Instances[0].InstanceId' \
-        --output text
+  --image-id "$AMI_ID" \
+  --instance-type "t3.micro" \
+  --security-group-ids "$SG_ID" \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+  --query 'Instances[0].InstanceId' \
+  --output text
         )          
 
     if [ $instance == "frontend" ]; then
