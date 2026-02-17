9b0bd057c6d368058ddbad5aab0eebad50a3c174
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-17 17:30:34 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,23 +5,27 @@
  
 for instance in $0
 do 
-         instance_id = $(aws ec2 run-instances \
-         --image-id $AMI_ID \
+         instance_id=$(aws ec2 run-instances \
+         --image-id "$AMI_ID" \
          --instance-type "t3.micro" \
-         --security-group-ids $SG_ID \
+         --security-group-ids "$SG_ID" \
          --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \ 
          --query 'Instances[0].InstanceId' \
-         --output text )
+         --output text )          
 
     if [ $instance == "frontend" ]; then
         
         IP = $(aws ec2 describe-instances \
-         --instance-ids $instance_id --query 'Reservations[].Instances[].PublicIpAddress' \
-         --output text )
+         --instance-ids $instance_id \
+         --query 'Reservations[].Instances[].PublicIpAddress' \
+         --output text 
+         )
     
     else
         IP = $(aws ec2 describe-instances \
-         --instance-ids $instance_id --query 'Reservations[].Instances[].PrivateIpAddress' \
-         --output text )
+         --instance-ids $instance_id \
+         --query 'Reservations[].Instances[].PrivateIpAddress' \
+         --output text 
+         )
    fi
 done
