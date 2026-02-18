6b8217e2d853b40fa77e77f777ddc6880cb67870
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-18 14:15:34 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e #ERR
+trap "echo 'this is error in line $LINENO,command $BASH_COMMAND'" ERR
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shellsriptforlooppkg_logs"
+LOGS_FILE=/var/log/shellsriptforlooppkg_logs/$0.log
+if [ $USERID -ne 0 ]; then
+echo -e "$R please run with root access $N" | tee -a $LOGS_FILE 
+exit 1
+fi
+mkdir  -p $LOGS_FOLDER
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo -e "$2 -----installation is $R failure $N" | tee -a $LOGS_FILE
+    exit 1
+    else 
+    echo -e "$2 ....installation is $G success $N" | tee -a $LOGS_FILE
+    fi
+}
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate $? " copying mongo repo file"
+
+dnf install mongodb-org -y 
+VALIDATE $? "mongodb installation on server"
+
+systemctl enable mongod 
+VALIDATE $? "enabling mongod service"
+
+systemctl start mongod 
+VALIDATE $? "starting mongod service"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "allowing remote connection to mongodb"
+
+systemctl restart mongod
+VALIDATE $? "restarted Mongodb" 
