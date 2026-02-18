3002792597716441703c2eeffc8e3d6815281418
pavankumar <pavankumar.puttolla@gmail.com>
2026-02-18 16:05:31 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -7,23 +7,25 @@
  
 for instance in $@
 do      
-   instance_id=$(aws ec2 run-instances \
-  --image-id "$AMI_ID" \
+   instance_id=$( aws ec2 run-instances \
+  --image-id $AMI_ID \
   --instance-type "t3.micro" \
-  --security-group-ids "$SG_ID" \
+  --security-group-ids $SG_ID \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
   --query 'Instances[0].InstanceId' \
   --output text )          
     if [ $instance == "frontend" ]; then
         
-        IP=$(aws ec2 describe-instances \
+        IP=$(
+            aws ec2 describe-instances \
          --instance-ids $instance_id \
          --query 'Reservations[].Instances[].PublicIpAddress' \
          --output text
          )
          RECORED_NAME="$DOMAIN_NAME"  #PSPK.ONLINE
     else
-        IP=$(aws ec2 describe-instances \
+        IP=$(
+           aws ec2 describe-instances \
          --instance-ids $instance_id \
          --query 'Reservations[].Instances[].PrivateIpAddress' \
          --output text
@@ -33,11 +35,12 @@ do
         echo "IP address:  $IP"
 
         aws route53 change-resource-record-sets \
-            --hosted-zone-id "$ZONE_ID" 
-            --change-batch '{
+            --hosted-zone-id $ZONE_ID \
+            --change-batch '
+            {
             "Comment": "Updating A record",
               "Changes": [
-              {
+                {
               "Action": "UPSERT",
               "ResourceRecordSet": {
               "Name": "'$RECORED_NAME'",
